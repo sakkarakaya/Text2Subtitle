d6b25690da90585d6968d5ef8c32f9e2f11fa171
mehmet-lgln <mehmet.karakaya@lgln.niedersachsen.de>
2025-03-04 11:28:18 +0100

    Initial Commit

---
+++ Text2Subtitle/Text2SubtitleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text2SubtitleApp.swift
+//  Text2Subtitle
+//
+//  Created by Mehmet Karakaya on 04.03.25.
+//
+
+import SwiftUI
+
+@main
+struct Text2SubtitleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Text2SubtitleTests/Text2SubtitleTests.swift
@@ -0,0 +1,17 @@
+//
+//  Text2SubtitleTests.swift
+//  Text2SubtitleTests
+//
+//  Created by Mehmet Karakaya on 04.03.25.
+//
+
+import Testing
+@testable import Text2Subtitle
+
+struct Text2SubtitleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
